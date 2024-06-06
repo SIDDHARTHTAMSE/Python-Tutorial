da5f9e4ac095338b58ae27139adfcdd66c6f7173
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-06 17:32:38 +0530

    printing statement, new line, concatinate, input, input function, Python variables,

---
+++ main.py
@@ -0,0 +1,53 @@
+print("A 'single quote' inside a double quote ")
+print('A "double quote" inside a single quote')
+print("Alternately you can just \"escape\" the quote")
+
+print("Hello World!")
+print("Hello World!\nHello world!\nHello World!")
+print("Hello" + "Sid")
+print("Hello" + " " +"Sid")
+
+#what is syntax error and indentation error
+#The word debugging means
+
+#The python input Function
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# Comment the code
+
+#[Interactive Coding Exercise] Input Function
+
+nameofletters = len("Siddharth")
+print(nameofletters)
+
+print(len(input()))
+
+
+
+# Python Variables
+
+name = input("What is your name?")
+print(name)
+
+
+name = "Sid"
+print(name)
+
+name = "manju"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+city = input("what is your city name\n")
+pet = input("What is your pet name\n")
+print("City name is "+city+" and pet name is "+pet)
+
+
+
+
+
