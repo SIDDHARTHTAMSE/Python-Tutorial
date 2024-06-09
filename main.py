285b1c7ba8022a3f8403afc04aeada9fe8311262
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-09 20:34:51 +0530

    Pyhton Primitive data types

--- main.py
+++ main.py
@@ -1,53 +1,102 @@
-print("A 'single quote' inside a double quote ")
-print('A "double quote" inside a single quote')
-print("Alternately you can just \"escape\" the quote")
-
-print("Hello World!")
-print("Hello World!\nHello world!\nHello World!")
-print("Hello" + "Sid")
-print("Hello" + " " +"Sid")
+# print("A 'single quote' inside a double quote ")
+# print('A "double quote" inside a single quote')
+# print("Alternately you can just \"escape\" the quote")
+#
+# print("Hello World!")
+# print("Hello World!\nHello world!\nHello World!")
+# print("Hello" + "Sid")
+# print("Hello" + " " +"Sid")
 
 #what is syntax error and indentation error
 #The word debugging means
 
 #The python input Function
 
-input("What is your name?")
-
-print("Hello " + input("What is your name?"))
+# input("What is your name?")
+#
+# print("Hello " + input("What is your name?"))
 
 # Comment the code
 
 #[Interactive Coding Exercise] Input Function
 
-nameofletters = len("Siddharth")
-print(nameofletters)
-
-print(len(input()))
+# nameofletters = len("Siddharth")
+# print(nameofletters)
+#
+# print(len(input()))
 
 
 
 # Python Variables
 
-name = input("What is your name?")
-print(name)
+# name = input("What is your name?")
+# print(name)
+#
+#
+# name = "Sid"
+# print(name)
+#
+# name = "manju"
+# print(name)
+#
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+#
+# city = input("what is your city name\n")
+# pet = input("What is your pet name\n")
+# print("City name is "+city+" and pet name is
+
+# print(len(input("What is Your name ? \n")))
+
+#Pyhton Primitive data types
+
+#String
+
+# print("Siddharth" [4])
+
+#Integer
+
+# print(12+45)
+
+#Large Number
+ # 12_34_98
+
+#Float
+
+# 1465.8
+
+#Boolean
+
+# True
+# False
+
+#
+# num_char = len(input("What is your name \n"))
+# print(type(num_char))
+
+# num_char_1 = "Hello"
+# print(type(num_char_1))
 
 
-name = "Sid"
-print(name)
 
-name = "manju"
-print(name)
+# num_char = len(input("What is your name \n"))
+# new_num_char = str(num_char)
+# print("My name has "+new_num_char+" characters")
 
-name = input("What is your name?")
-length = len(name)
-print(length)
+# print(70 + float("100.5"))
 
-city = input("what is your city name\n")
-pet = input("What is your pet name\n")
-print("City name is "+city+" and pet name is "+pet)
+# print(str(70) + str(100))
 
 
+# two_digit_number = input()
+#
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+#
+# three_digit_number = first_digit + second_digit
+#
+# print(three_digit_number)
 
-
+# Mathematical Operation in Python
 
