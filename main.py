85d861247ff1e53d2b7e13055d48509fb39615bb
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-10 17:57:56 +0530

    Calculate BMI value,Number Manipulation and F Strings in Python,How many years you have left in your life program,Calculator program,Control Flow with if / else and Conditional Operators,Odd and Even Program

--- main.py
+++ main.py
@@ -74,7 +74,7 @@
 #
 # num_char = len(input("What is your name \n"))
 # print(type(num_char))
-
+#
 # num_char_1 = "Hello"
 # print(type(num_char_1))
 
@@ -105,3 +105,81 @@
 # */
 # +-
 
+# height = input()
+# weight = input()
+#
+# weight_as_int = int(weight)
+# height_as_float = float(height)
+
+# using the exponent operator **
+# bmi = weight_as_int / height_as_float ** 2
+
+# or using multiplication and PEMDAS
+# bmi = weight_as_int / (height_as_float * height_as_float)
+#
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
+#
+
+# Number Manipulation and F Strings in Python
+# print(round(8/3, 3))
+# print(int(8/3))
+#
+
+# score = 0
+# height = 1.8
+# isWinning = True
+#
+# # f-String
+# print(f"Your score is {score},"
+#       f" your height is {height},"
+#       f" you are winning is {isWinning}"
+#       )
+
+
+# How many years you have left in your life program
+# age = input()
+# years = 90 - int(age)
+# weeks = years * 52
+# print(f"you have {weeks} weeks left.")
+
+# age = input()
+# years = 90 - int(age)
+# days = years * 365
+# print(F"You have total {days} days left in your life")
+#
+
+# name = input("What is your name ?")
+# print("Hello, "+name)
+
+# print("Welcome to the tip Calculator !")
+# bill = float(input("What was the total bill ? \n"))
+# tips = int(input("How much tip would you like to give ? \n"))
+# people = int(input("How many people to split the bill ?\n"))
+# tip_as_percent = tips / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+# final_amount1 = "{:.2f}".format(bill_per_person)
+# print(f"Each person should pay {final_amount1}")
+
+# Control Flow with if / else and Conditional Operators
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm \n"))
+#
+# if height != 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# Odd and Even
+
+# print("Which number do you want to check?")
+# number = int(input())
+#
+# if number % 2 == 0:
+#     print("This is even number")
+# else:
+#     print("This is odd number")
