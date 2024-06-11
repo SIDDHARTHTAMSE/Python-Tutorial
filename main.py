bcba5ce2e3291ce0ded1995cb9f01fcc69801678
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-11 17:57:07 +0530

    Nested if statements and elif statements,BMI 2.0 program,Leap year or not Program,Multiple If Statements in Succession,Pizza Program

--- main.py
+++ main.py
@@ -138,20 +138,21 @@
 
 
 # How many years you have left in your life program
+#
 # age = input()
 # years = 90 - int(age)
 # weeks = years * 52
-# print(f"you have {weeks} weeks left.")
-
 # age = input()
+# print(f"you have {weeks} weeks left.")
+#
 # years = 90 - int(age)
 # days = years * 365
 # print(F"You have total {days} days left in your life")
 #
-
+#
 # name = input("What is your name ?")
 # print("Hello, "+name)
-
+#
 # print("Welcome to the tip Calculator !")
 # bill = float(input("What was the total bill ? \n"))
 # tips = int(input("How much tip would you like to give ? \n"))
@@ -163,9 +164,9 @@
 # final_amount = round(bill_per_person, 2)
 # final_amount1 = "{:.2f}".format(bill_per_person)
 # print(f"Each person should pay {final_amount1}")
-
+#
 # Control Flow with if / else and Conditional Operators
-
+#
 # print("Welcome to the rollercoaster!")
 # height = int(input("What is your height in cm \n"))
 #
@@ -173,9 +174,9 @@
 #     print("You can ride the rollercoaster")
 # else:
 #     print("Sorry, you have to grow taller before you can ride.")
-
+#
 # Odd and Even
-
+#
 # print("Which number do you want to check?")
 # number = int(input())
 #
@@ -183,3 +184,116 @@
 #     print("This is even number")
 # else:
 #     print("This is odd number")
+
+# Nested if statements and elif statements
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+age = int(input("What is your age? \n"))
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    if age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry, You have to grow taller before you can ride.")
+
+
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+age = int(input("What is your age? \n"))
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry, You have to grow taller before you can ride.")
+
+# BMI 2.0
+
+height = float(input())
+weight = int((input()))
+bmi = weight / (height * height)
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you are normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+# Leap year or not
+
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
+
+
+#Multiple If Statements in Succession
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+age = int(input("What is your age? \n"))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken ? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+    print(f"Your final bill is ${bill}")
+else:
+    print("Sorry, You have to grow taller before you can ride.")
+
+# Pizza
+
+print("Thank you for choosing Python Pizza deliveries!\n")
+size = input("What size pizza do you want? S,M or L\n")
+add_pepperoni = input("Do you want pepperoni? Y or N\n")
+extra_cheese = input("Do you want extra chees? Y or N\n")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}")
