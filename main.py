3188deb91018fd6c416ecde3f40c7fb9fbca518a
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-12 18:23:54 +0530

    Logical Operators,Love Calculator,Treasure Island project, Random Module and list

--- main.py
+++ main.py
@@ -187,68 +187,118 @@
 
 # Nested if statements and elif statements
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? \n"))
-age = int(input("What is your age? \n"))
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    if age <= 18:
-        print("Please pay $7.")
-    else:
-        print("Please pay $12")
-else:
-    print("Sorry, You have to grow taller before you can ride.")
-
-
-
-
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? \n"))
-age = int(input("What is your age? \n"))
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    if age < 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")
-    else:
-        print("Please pay $12")
-else:
-    print("Sorry, You have to grow taller before you can ride.")
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? \n"))
+# age = int(input("What is your age? \n"))
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     if age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12")
+# else:
+#     print("Sorry, You have to grow taller before you can ride.")
+#
+#
+#
+#
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? \n"))
+# age = int(input("What is your age? \n"))
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12")
+# else:
+#     print("Sorry, You have to grow taller before you can ride.")
 
 # BMI 2.0
 
-height = float(input())
-weight = int((input()))
-bmi = weight / (height * height)
-if bmi < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi < 25:
-    print(f"Your BMI is {bmi}, you are normal weight.")
-elif bmi < 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi < 35:
-    print(f"Your BMI is {bmi}, you are obese")
-else:
-    print(f"Your BMI is {bmi}, you are clinically obese.")
+# height = float(input())
+# weight = int((input()))
+# bmi = weight / (height * height)
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi < 25:
+#     print(f"Your BMI is {bmi}, you are normal weight.")
+# elif bmi < 30:
+#     print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi < 35:
+#     print(f"Your BMI is {bmi}, you are obese")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese.")
 
 # Leap year or not
-
-year = int(input())
-
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap Year")
-        else:
-            print("Not leap year")
-    else:
-        print("Leap year")
-else:
-    print("Not leap year")
+#
+# year = int(input())
+#
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap Year")
+#         else:
+#             print("Not leap year")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not leap year")
 
 
 #Multiple If Statements in Succession
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? \n"))
+# age = int(input("What is your age? \n"))
+# bill = 0
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     if age < 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+#
+#     wants_photo = input("Do you want a photo taken ? Y or N. ")
+#     if wants_photo == "Y":
+#         bill += 3
+#     print(f"Your final bill is ${bill}")
+# else:
+#     print("Sorry, You have to grow taller before you can ride.")
+
+# Pizza
+
+# print("Thank you for choosing Python Pizza deliveries!\n")
+# size = input("What size pizza do you want? S,M or L\n")
+# add_pepperoni = input("Do you want pepperoni? Y or N\n")
+# extra_cheese = input("Do you want extra chees? Y or N\n")
+# bill = 0
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# else:
+#     bill += 25
+#
+# if add_pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+#
+# if extra_cheese == "Y":
+#     bill += 1
+#
+# print(f"Your final bill is: ${bill}")
+
+#Logical Operators
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? \n"))
@@ -262,38 +312,83 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         bill = 12
         print("Adult tickets are $12.")
 
-    wants_photo = input("Do you want a photo taken ? Y or N. ")
+    wants_photo = input("Do you want a photo taken? Y or N \n")
     if wants_photo == "Y":
         bill += 3
     print(f"Your final bill is ${bill}")
 else:
     print("Sorry, You have to grow taller before you can ride.")
 
-# Pizza
+#Love Calculator
 
-print("Thank you for choosing Python Pizza deliveries!\n")
-size = input("What size pizza do you want? S,M or L\n")
-add_pepperoni = input("Do you want pepperoni? Y or N\n")
-extra_cheese = input("Do you want extra chees? Y or N\n")
-bill = 0
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
+print("The Love Calculator is calculating your score....")
+name1 = input("What is your name?\n")
+name2 = input("What is their name?\n")
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+if (score < 10) or (score > 90):
+    print(f"your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"your score is {score}, you are alright together.")
 else:
-    bill += 25
+    print(f"your score is {score}.")
 
-if add_pepperoni == "Y":
-    if size == "S":
-        bill += 2
+
+# Treasure Island project
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You have at a crossroad, where do you want to go? Type "left" or "right".\n').lower()
+
+if choice1 == "left":
+    choice2 = input('You have to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a with 3 doors. One red, one yellow and one blue.\n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
     else:
-        bill += 3
+        print("You got attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
 
-if extra_cheese == "Y":
-    bill += 1
+# Random Module
+import random
+random_integer = random.randint(1, 10)
+print(random_integer)
 
-print(f"Your final bill is: ${bill}")
+random_float = random.random()
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Head")
+else:
+    print("Tail")
