852b814960c7f56027edbf456a8759f18bab2985
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-26 22:24:07 +0530

    Local vs. Global Scope,Local scope,There is no Block Scope,How to Modify a Global Variable,The Number Guessing program

--- main.py
+++ main.py
@@ -1266,79 +1266,189 @@ import random
 # calculator()
 
 # Blackjack program
-from replit import clear
-from art import logo
+
+# from replit import clear
+# from art import logo
+#
+#
+# def deal_card():
+#     """Returns a random card from the deck."""
+#     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+#     card = random.choice(cards)
+#     return card
+#
+#
+# def calculate_score(cards):
+#     """Take a list of cards and return the score calculated from the cards"""
+#     if sum(cards) == 21 and len(cards) == 2:
+#         return 0
+#
+#     if 11 in cards and sum(cards) > 21:
+#         cards.remove(11)
+#         cards.append(1)
+#     return sum(cards)
+#
+#
+# def compare(user_score, computer_score):
+#     if user_score == computer_score:
+#         return "Draw"
+#     elif computer_score == 0:
+#         return "Lose, opponent has Blackjack"
+#     elif user_score == 0:
+#         return "Win with a Blackjack"
+#     elif user_score > 21:
+#         return "You went over. You lose"
+#     elif computer_score > 21:
+#         return "Opponent went over. You win"
+#     elif user_score > computer_score:
+#         return "You win"
+#     else:
+#         return "You lose"
+#
+#
+# def play_game():
+#     print(logo)
+#     user_cards = []
+#     computer_cards = []
+#     is_game_over = False
+#
+#     for _ in range(2):
+#         user_cards.append(deal_card())
+#         computer_cards.append(deal_card())
+#
+#     while not is_game_over:
+#         user_score = calculate_score(user_cards)
+#         computer_score = calculate_score(computer_cards)
+#         print(f"   Your cards: {user_cards}, current score: {user_score}")
+#         print(f"   Computer's first card: {computer_cards[0]}")
+#
+#         if user_score == 0 or computer_score == 0 or user_score > 21:
+#             is_game_over = True
+#         else:
+#             user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+#             if user_should_deal == "y":
+#                 user_cards.append(deal_card())
+#             else:
+#                 is_game_over = True
+#
+#     while computer_score != 0 and computer_score < 17:
+#         computer_cards.append(deal_card())
+#         computer_score = calculate_score(computer_cards)
+#
+#     print(f"  Your final hand: {user_cards}, final score: {user_score}")
+#     print(f" Computer's final hand: {computer_cards}, final score: {computer_score}")
+#     print(compare(user_score, computer_score))
+#
+#
+# while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+#     clear()
+#     play_game()
+
+# Namespaces: Local vs. Global Scope
+
+enemies = 1
 
 
-def deal_card():
-    """Returns a random card from the deck."""
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
+def increase_enemies():
+    enemies = 2
+    print(f" enemies inside function: {enemies}")
 
 
-def calculate_score(cards):
-    """Take a list of cards and return the score calculated from the cards"""
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
+increase_enemies()
+print(f" enemies outside function: {enemies}")
 
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
+# Local scope
+
+def drink_position():
+    position_strength = 2
+    print(position_strength)
 
 
-def compare(user_score, computer_score):
-    if user_score == computer_score:
-        return "Draw"
-    elif computer_score == 0:
-        return "Lose, opponent has Blackjack"
-    elif user_score == 0:
-        return "Win with a Blackjack"
-    elif user_score > 21:
-        return "You went over. You lose"
-    elif computer_score > 21:
-        return "Opponent went over. You win"
-    elif user_score > computer_score:
-        return "You win"
+drink_position()
+
+Global variable
+
+player_health = 10
+
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+
+
+drink_potion()
+print(player_health)
+
+# There is no Block Scope
+
+game_level = 3
+
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
+
+# How to Modify a Global Variable
+
+enemies = 1
+
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+
+enymy = increase_enemies()
+print(f"enemies outside function: {enymy}")
+
+# The Number Guessing Game
+
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+
+def check_answer(guess, answer, turns):
+    """checks answer against guess. Return the number of turns remaining."""
+    if guess > answer:
+        print("Too high")
+        return turns - 1
+    elif guess < answer:
+        print("Too low")
+        return turns - 1
     else:
-        return "You lose"
+        print(f"You got it! The answer was {answer}.")
 
 
-def play_game():
-    print(logo)
-    user_cards = []
-    computer_cards = []
-    is_game_over = False
-
-    for _ in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
-
-    while not is_game_over:
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
-        print(f"   Your cards: {user_cards}, current score: {user_score}")
-        print(f"   Computer's first card: {computer_cards[0]}")
-
-        if user_score == 0 or computer_score == 0 or user_score > 21:
-            is_game_over = True
-        else:
-            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
-            if user_should_deal == "y":
-                user_cards.append(deal_card())
-            else:
-                is_game_over = True
-
-    while computer_score != 0 and computer_score < 17:
-        computer_cards.append(deal_card())
-        computer_score = calculate_score(computer_cards)
-
-    print(f"  Your final hand: {user_cards}, final score: {user_score}")
-    print(f" Computer's final hand: {computer_cards}, final score: {computer_score}")
-    print(compare(user_score, computer_score))
+def set_defficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
 
 
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
-    clear()
-    play_game()
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    answer = randint(1, 100)
+
+    turns = set_defficulty()
+
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess:"))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("you've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again.")
+
+
+game()
