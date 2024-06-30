fb22baab1506b61a7678b4a38a9603c85ee3f42a
Siddarth Tamse <sidtamse7@gmail.com>
2024-06-30 21:56:27 +0530

    Jay Shree Ram Program

--- main.py
+++ main.py
@@ -1631,99 +1631,133 @@ import random
 
 # Coffee Machine Code
 
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
-
-def is_resource_sufficient(order_ingredients):
-    """Returns True when order can be made, False if ingredients are insufficient"""
-    for item in order_ingredients:
-        if order_ingredients[item] >= resources[item]:
-            print(f"Sorry there is not enough {item}")
-            return False
-    return True
-
-
-def process_coins():
-    """Return the total calculated from coins inserted."""
-    print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.23
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
-
-
-def is_transaction_successful(money_received, drink_cost):
-    """Return True when payment is accepted, or False if money is insufficient."""
-    if money_received >= drink_cost:
-        change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change.")
-        global profit
-        profit += drink_cost
-        return True
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-        return False
-
-def make_coffe(drink_name, order_ingredients):
-    """Deduct the required ingredients from the resources"""
-    for item in order_ingredients:
-        resources[item] -= order_ingredients[item]
-    print(f"Here is your {drink_name}")
-
-is_on = True
-
-while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: &{profit}")
-    else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffe(choice, drink["ingredients"])
-
-
-
-
+# MENU = {
+#     "espresso": {
+#         "ingredients": {
+#             "water": 50,
+#             "coffee": 18,
+#         },
+#         "cost": 1.5,
+#     },
+#     "latte": {
+#         "ingredients": {
+#             "water": 200,
+#             "milk": 150,
+#             "coffee": 24,
+#         },
+#         "cost": 2.5,
+#     },
+#     "cappuccino": {
+#         "ingredients": {
+#             "water": 250,
+#             "milk": 100,
+#             "coffee": 24,
+#         },
+#         "cost": 3.0,
+#     }
+# }
+#
+# profit = 0
+# resources = {
+#     "water": 300,
+#     "milk": 200,
+#     "coffee": 100,
+# }
+#
+#
+# def is_resource_sufficient(order_ingredients):
+#     """Returns True when order can be made, False if ingredients are insufficient"""
+#     for item in order_ingredients:
+#         if order_ingredients[item] >= resources[item]:
+#             print(f"Sorry there is not enough {item}")
+#             return False
+#     return True
+#
+#
+# def process_coins():
+#     """Return the total calculated from coins inserted."""
+#     print("Please insert coins.")
+#     total = int(input("how many quarters?: ")) * 0.23
+#     total += int(input("how many dimes?: ")) * 0.1
+#     total += int(input("how many nickles?: ")) * 0.05
+#     total += int(input("how many pennies?: ")) * 0.01
+#     return total
+#
+#
+# def is_transaction_successful(money_received, drink_cost):
+#     """Return True when payment is accepted, or False if money is insufficient."""
+#     if money_received >= drink_cost:
+#         change = round(money_received - drink_cost, 2)
+#         print(f"Here is ${change} in change.")
+#         global profit
+#         profit += drink_cost
+#         return True
+#     else:
+#         print("Sorry that's not enough money. Money refunded.")
+#         return False
+#
+#
+# def make_coffe(drink_name, order_ingredients):
+#     """Deduct the required ingredients from the resources"""
+#     for item in order_ingredients:
+#         resources[item] -= order_ingredients[item]
+#     print(f"Here is your {drink_name}")
+#
+#
+# is_on = True
+#
+# while is_on:
+#     choice = input("What would you like? (espresso/latte/cappuccino): ")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         print(f"Water: {resources['water']}ml")
+#         print(f"Milk: {resources['milk']}ml")
+#         print(f"Coffee: {resources['coffee']}g")
+#         print(f"Money: &{profit}")
+#     else:
+#         drink = MENU[choice]
+#         if is_resource_sufficient(drink["ingredients"]):
+#             payment = process_coins()
+#             if is_transaction_successful(payment, drink["cost"]):
+#                 make_coffe(choice, drink["ingredients"])
 
 
+# Jay Shree Ram
+
+
+from turtle import *
+
+title("Jai Shree Ram")
+bgcolor('black')
+pensize(5)
+pencolor("orange")
+
+Ram_naam = [
+    "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+]
+
+angle = 350/30
+penup()
+sety(-1)
+for i in range(len(Ram_naam)):
+    left(angle)
+    forward(260)
+    write(Ram_naam[i], align="right", font=('Arial', 12, "bold"))
+    backward(260)
+
+penup()
+goto(-40, -20)
+pendown()
+write("|| राम ||", font=("Arial", 60, "normal"), align="center")
+hideturtle()
+done()
 
 
