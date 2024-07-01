95d5bb8aae007979e766902f7ff835f3a99511f7
Siddarth Tamse <sidtamse7@gmail.com>
2024-07-01 18:08:13 +0530

    Object Oriented Programming, Create a table using PrettyTable, object creation, Building the Coffee Machine in OOP

--- main.py
+++ main.py
@@ -1726,38 +1726,89 @@ import random
 # Jay Shree Ram
 
 
-from turtle import *
+# from turtle import *
+#
+# title("Jai Shree Ram")
+# bgcolor('black')
+# pensize(5)
+# pencolor("orange")
+#
+# Ram_naam = [
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+#     "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
+# ]
+#
+# angle = 350/30
+# penup()
+# sety(-1)
+# for i in range(len(Ram_naam)):
+#     left(angle)
+#     forward(260)
+#     write(Ram_naam[i], align="right", font=('Arial', 12, "bold"))
+#     backward(260)
+#
+# penup()
+# goto(-40, -20)
+# pendown()
+# write("|| राम ||", font=("Arial", 60, "normal"), align="center")
+# hideturtle()
+# done()
 
-title("Jai Shree Ram")
-bgcolor('black')
-pensize(5)
-pencolor("orange")
+# Object Oriented Programming
 
-Ram_naam = [
-    "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-    "जय श्री राम", "जय श्री राम", "जय श्री राम", "जय श्री राम",
-]
+import another_module
+print(another_module.another_variable)
 
-angle = 350/30
-penup()
-sety(-1)
-for i in range(len(Ram_naam)):
-    left(angle)
-    forward(260)
-    write(Ram_naam[i], align="right", font=('Arial', 12, "bold"))
-    backward(260)
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
-penup()
-goto(-40, -20)
-pendown()
-write("|| राम ||", font=("Arial", 60, "normal"), align="center")
-hideturtle()
-done()
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+# Create a table using PrettyTable
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
+# Building the Coffee Machine in OOP
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
 
 
