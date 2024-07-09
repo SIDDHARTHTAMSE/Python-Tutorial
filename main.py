26b7a117636c3ce811ad8ff2b4be25631e4d2ebb
Siddarth Tamse <sidtamse7@gmail.com>
2024-07-09 20:11:32 +0530

    Generate a Random Walk

--- main.py
+++ main.py
@@ -1942,6 +1942,36 @@ import random
 
 # Drawing Different Shapes
 
+# import turtle as t
+#
+#
+# tim = t.Turtle()
+#
+# colors = [
+#     "medium aquamarine",
+#     "blue",
+#     "dark slate gray",
+#     "green",
+#     "yellow",
+#     "maroon",
+#     "purple"
+# ]
+
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+#
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
+
+
+# Generate a Random Walk
+
 import turtle as t
 
 
@@ -1957,15 +1987,11 @@ colors = [
     "purple"
 ]
 
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-
-for shape_side_n in range(3, 11):
+for _ in range(200):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
-
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
